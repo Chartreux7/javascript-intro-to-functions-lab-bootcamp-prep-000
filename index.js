0a50e7741f2e4ce134e2b48e73205e03cb000602
Jessica Bailey <baileyjessica2@gmail.com>
2017-08-31 19:03:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,6 +24,6 @@ function sayHiToGrandma (string) {
     return ("YES INDEED!")
   }
   else if (string === "I love you, Grandma.") {
-    console.log("I love you, too.")
+    return ("I love you, too.")
   }
 }
