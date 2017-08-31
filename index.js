ecc65f750b0607586ca8edff47a902cf2d4e511b
Jessica Bailey <baileyjessica2@gmail.com>
2017-08-31 18:51:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ function logShout(string) {
 function logWhisper(string) {
   console.log(string.toLowerCase())
 }
-if (string === lowercase) {
-  return ("I can't hear you!")
+function sayHiToGrandma (string) {
+  if (string === lowercase) {
+    return ("I can't hear you!")
+  }
 }
